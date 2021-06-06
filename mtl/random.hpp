84c89b4cd24c1c75f1282708cd8d909e66c9875f
Michael Trikergiotis <34986166+MichaelTrikergiotis@users.noreply.github.com>
2021-06-06 14:59:33 +0300

    Changed some comments.

--- mtl/random.hpp
+++ mtl/random.hpp
@@ -1,12 +1,15 @@
 #pragma once
-// random by Michael Trikergiotis
+// random header by Michael Trikergiotis
 // 22/10/2017
 // 
-// Header for algorithms related to random number generation.
+//
+// This header contains algorithms related to random number generation.
+//
 // 
 // Copyright (c) Michael Trikergiotis. All Rights Reserved.
 // Licensed under the MIT license. See LICENSE in the project root for license information. 
 // See ThirdPartyNotices.txt in the project root for third party licenses information.
+
 
 #include "definitions.hpp"      // various definitions
 #include <random>				// std::random_device, std::uniform_int_distribution
