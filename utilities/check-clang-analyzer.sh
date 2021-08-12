526c0b972791615d66d6f2aa3ce5834fca4a0860
Michael Trikergiotis <34986166+MichaelTrikergiotis@users.noreply.github.com>
2021-08-12 17:30:37 +0300

    Improved a script.

--- utilities/check-clang-analyzer.sh
+++ utilities/check-clang-analyzer.sh
@@ -2,18 +2,33 @@
 
 # Runs clang-analyzer to check mtl for bugs.
 #
-# You must have clang and clang-tools installed for this script to work.
+# You must have clang, clang-tools, CMake and make installed for this script to work.
 # On Ubuntu Linux :
-# sudo apt install clang clang-tools
+# sudo apt install clang clang-tools cmake make
 
 
 
 
 
-# delete the generated executable if it existed
-rm -f tests
+
+# delete the build directory if it exists
+rm -rf build-clang-analyzer
+# create the build directory
+mkdir build-clang-analyzer
+cd build-clang-analyzer
+
+
+# the scan-build command line utility allows us to setup and run the clang analyzer
+
+echo "Configuring clang analyzer..."
+# use scan-build to run CMake, set clang as the compiler
+scan-build --use-cc=clang --use-c++=clang++ cmake ../../tests/ -DENABLE_CONSOLE_TESTS=ON
+
+echo ""
 echo "Running clang analyzer..."
-# run clang-analyzer using the scan-build command line utility
-scan-build --exclude ../mtl/fmt/ clang++ -o tests -std=c++17 ../tests/*.cpp
-# delete the generated executable
-rm -f tests
+# use scan-build to build with make, make will use all cores, set clang as the compiler
+scan-build --use-cc=clang --use-c++=clang++ make -j$(nproc)
+
+# delete the build directory
+cd ..
+rm -rf build-clang-analyzer
