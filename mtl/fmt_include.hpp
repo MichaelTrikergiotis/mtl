24bb0da83e02182f0ef0f10cf06b5b7de5623cde
Michael Trikergiotis <34986166+MichaelTrikergiotis@users.noreply.github.com>
2021-08-12 13:27:51 +0300

    Minor formatting changes.

--- mtl/fmt_include.hpp
+++ mtl/fmt_include.hpp
@@ -3,7 +3,8 @@
 // 04/06/2020
 // 
 // 
-// Header that just includes fmt library and supresses the multiple warnings related with it.
+// Header that includes and configures the fmt library. It also supresses multiple warnings 
+// related with it.
 // 
 // 
 // Copyright (c) Michael Trikergiotis. All Rights Reserved.
@@ -11,20 +12,22 @@
 // See ThirdPartyNotices.txt in the project root for third party licenses information.
 
 
+
+
 // supress MSVC warnings
 #if defined(_MSC_VER)
 #pragma warning( push )
-#pragma warning( disable : 26498 )
-#pragma warning( disable : 26495 )
-#pragma warning( disable : 26439 )
+#pragma warning( disable : 6285  )
+#pragma warning( disable : 6323  )
+#pragma warning( disable : 6385  )
 #pragma warning( disable : 26437 )
+#pragma warning( disable : 26439 )
 #pragma warning( disable : 26451 )
 #pragma warning( disable : 26495 )
+#pragma warning( disable : 26498 )
 #pragma warning( disable : 26812 )
-#pragma warning( disable : 6385  )
-#pragma warning( disable : 6323 )
-#pragma warning( disable : 6285 )
 #endif // _MSC_VER end
+
 
 // supress GCC warnings
 #if defined(__GNUC__) 
@@ -37,12 +40,15 @@
 #endif // __clang__ end
 #endif // __GNUC__ end
 
+
 // supress clang warnings
 #if defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wshadow"
 #pragma clang diagnostic ignored "-Wsign-conversion"
 #endif // __clang__ end 
+
+
 
 
 
@@ -67,10 +73,16 @@
 #endif // MTL_EXTERNALLY_SUPPLIED_FMT
 
 
+
+
+
+
+
 // stop supressing clang warnings
 #if defined(__clang__)
 #pragma clang diagnostic pop
 #endif // __clang__ end
+
 
 // stop suppressing MSVC warnings
 #if defined(_MSC_VER)
